2ffdf6484624f98c6017f5eb0033c8d15a648c15
laan71 <lail5198@edu.easj.dk>
2020-09-11 11:29:59 +0200

    I have written me name in our Repo.

--- src/Faelles_repos.java
+++ src/Faelles_repos.java
@@ -1,8 +1,13 @@
+import java.sql.SQLOutput;
+
 public class Faelles_repos {
     public static void main(String[] args) {
         System.out.println("This is our first shared repo :)");
         System.out.println("Hello This is Jenna");
         System.out.print("Jenna");
+
+        System.out.println("Hello This is Laila");
+        System.out.println("Laila");
     }
 
 
